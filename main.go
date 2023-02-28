7b1e1251de319c7449393514c6fc05dc6baac9fe
MMMohebi <MMMohebi@outlook.com>
2023-02-28 12:28:16 +0330

    init in web server mode

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"telegram-bots-forwarder/common"
+	"telegram-bots-forwarder/configs"
+	"telegram-bots-forwarder/router"
+)
+
+// nodemon --exec go run main.go --signal SIGTERM
+
 func main() {
-	println("hi")
+	configs.Setup()
+
+	server := gin.Default()
+
+	router.Routs(server)
+
+	err := server.Run("localhost:8005")
+	common.IsErr(err, "Err in starting server")
 }
